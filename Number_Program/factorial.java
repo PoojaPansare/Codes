fc17d6068dd614ffd74dd6427a3050642d142162
Pooja Pansare <136827234+PoojaPansare@users.noreply.github.com>
2024-11-20 15:37:23 +0530

    Add files via upload

---
+++ Number_Program/factorial.java
@@ -0,0 +1,31 @@
+class factorial 
+{
+	public static void main(String[] args) 
+	{
+		int n = 4;
+		int fact = 1;
+		while(n != 0)
+		{
+			fact = fact * n;
+			n --;
+		}
+		System.out.println(fact);
+	}
+}
+
+
+// class factorial
+//{
+//	public static void main(String[] args)
+//	{
+//		 int n = 4;
+//		int fact = 1;
+//		for(int i=1; i<=n; i++)
+//		{
+//			fact = fact * i;
+//		}
+//		System.out.println(fact);
+
+//	}
+
+
